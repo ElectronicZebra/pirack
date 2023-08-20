78b8d1d003eb8a6f240c6c82a7afbe918f0b98c1
ElectronicZebra <rajesh130883@gmail.com>
2023-08-19 22:15:04 -0700

    version 1.0 commit

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 //https://www.npmjs.com/package/oled-i2c-bus
+var ip = require("ip");
+var os = require("os");
+const si = require('systeminformation');
 var i2c = require('i2c-bus'),
     i2cBus = i2c.openSync(1),
     oled = require('oled-i2c-bus');
-const font = require('oled-font-pack');
+var font = require('oled-font-5x7');
+var fs = require('fs');
+var obj = JSON.parse(fs.readFileSync('settings.json', 'utf8'));
+console.dir(obj);
 
 var opts = {
     width: 128,
@@ -10,10 +16,97 @@ var opts = {
     address: 0x3C
 };
 
+const TOTAL_MESSAGES = 5;
+const MESSAGE_DISPLAY_MILLISECONDS = 5000;
+
 var display = new oled(i2cBus, opts);
 display.clearDisplay();
 display.stopScroll();
 display.turnOnDisplay();
-display.setCursor(0, 0);
-display.fillRect(1, 1, 10, 30, 1);
-display.startScroll('left', 0, 15);
+
+// message rotation
+var current_message = 1;
+setInterval(() => {
+
+    switch (current_message) {
+        case 1:
+            dispAppDetails();
+            break;
+        case 2:
+            dispTemps();
+            break;
+        case 3:
+            dispCPUInfo();
+            break;
+        case 4:
+            dispMEMInfo();
+            break;
+        case 5:
+            dispDISKInfo();
+            break;
+    }
+
+    if (current_message == TOTAL_MESSAGES) {
+        current_message = 1;
+    } else {
+        current_message++;
+    }
+}, MESSAGE_DISPLAY_MILLISECONDS);
+
+
+dispAppDetails = function () {
+    display.clearDisplay();
+    display.setCursor(1, 1);
+    display.writeString(font, 2, obj.appName, 1, false);
+    display.setCursor(1, 20);
+    display.writeString(font, 1, os.hostname() + ' ' + ip.address(), 1, false);
+}
+
+dispTemps = function () {
+    display.clearDisplay();
+    si.cpuTemperature()
+        .then(function (data) {
+            display.setCursor(1, 1);
+            display.writeString(font, 2, 'TEMP : ' + Math.round(data.main) + ' C', 1, false);
+            display.setCursor(1, 18);
+            display.writeString(font, 2, 'MAX  : ' + Math.round(data.max) + ' C', 1, false);
+
+        })
+        .catch(error => console.error(error));
+}
+
+dispCPUInfo = function () {
+    display.clearDisplay();
+    si.currentLoad()
+        .then(function (data) {
+            display.setCursor(1, 1);
+            display.writeString(font, 2, 'CPU : ' + Math.round(data.currentLoad) + ' %', 1, false);
+            display.setCursor(1, 18);
+            display.writeString(font, 2, 'IDLE : ' + Math.round(data.currentLoadIdle) + ' %', 1, false);
+        })
+        .catch(error => console.error(error));
+}
+
+dispMEMInfo = function () {
+    display.clearDisplay();
+    si.mem()
+        .then(function (data) {
+            display.setCursor(1, 1);
+            display.writeString(font, 2, 'MEM : ' + Math.round((data.active / data.total) * 100) + ' %', 1, false);
+            display.setCursor(1, 18);
+            display.writeString(font, 2, 'SWP : ' + Math.round((data.swapused / data.swaptotal) * 100) + ' %', 1, false);
+        })
+        .catch(error => console.error(error));
+}
+
+dispDISKInfo = function () {
+    display.clearDisplay();
+    si.fsSize()
+        .then(function (data) {
+            display.setCursor(1, 1);
+            display.writeString(font, 2, 'U : ' + (data[0].used / (1024 * 1024 * 1024)).toFixed(2) + 'G', 1, false);
+            display.setCursor(1, 18);
+            display.writeString(font, 2, 'T : ' + (data[0].size / (1024 * 1024 * 1024)).toFixed(2) + 'G', 1, false);
+        })
+        .catch(error => console.error(error));
+}
